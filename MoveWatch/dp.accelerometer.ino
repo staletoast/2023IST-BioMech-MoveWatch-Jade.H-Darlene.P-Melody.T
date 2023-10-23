b31fd0860d6cc7673b59226c9799098a6a293cce
Darlene Purnomo <darlene.purnomo@education.nsw.gov.au>
2023-10-23 14:49:23 +1100

    deleted code

--- MoveWatch/dp.accelerometer.ino
+++ MoveWatch/dp.accelerometer.ino
@@ -1,54 +1,7 @@
-/*
-  Purpose: Basic example of reading data from the 3-Axis Accelerometer.
-  Notes:
-    1. Connect to I2C - Default 0x19, can be changed to 0x18 when connecting SDO Pin with GND
-    2. Measurement range: ±2g, ±4g, ±8g, ±16g, multiple ranges selection.
-  Author: Ben Jones 14/7/23
-  Contact: benjmain.jones21@det.nsw.edu.au
-  Source: https://wiki.seeedstudio.com/Grove-3-Axis-Digital-Accelerometer-LIS3DHTR/
-  Library Source: https://github.com/Seeed-Studio/Seeed_Arduino_LIS3DHTR
-*/
 
-// This example use I2C.
-#include "LIS3DHTR.h"
-#include <Wire.h>
-LIS3DHTR<TwoWire> LIS; //IIC
-#define WIRE Wire
 
-void setup()
-{
-  Serial.begin(9600);
-  LIS.begin(WIRE,0x19); //Configure wire communciation to 0x19 address
-  LIS.openTemp();
-  delay(100);
-    LIS.setFullScaleRange(LIS3DHTR_RANGE_2G);
-  //  LIS.setFullScaleRange(LIS3DHTR_RANGE_4G);
-  //  LIS.setFullScaleRange(LIS3DHTR_RANGE_8G);
-  //  LIS.setFullScaleRange(LIS3DHTR_RANGE_16G);
-  //  LIS.setOutputDataRate(LIS3DHTR_DATARATE_1HZ);
-  //  LIS.setOutputDataRate(LIS3DHTR_DATARATE_10HZ);
-  //  LIS.setOutputDataRate(LIS3DHTR_DATARATE_25HZ);
-  LIS.setOutputDataRate(LIS3DHTR_DATARATE_50HZ);
-  //  LIS.setOutputDataRate(LIS3DHTR_DATARATE_100HZ);
-  //  LIS.setOutputDataRate(LIS3DHTR_DATARATE_200HZ);
-  //  LIS.setOutputDataRate(LIS3DHTR_DATARATE_1_6KHZ);
-  //  LIS.setOutputDataRate(LIS3DHTR_DATARATE_5KHZ);
-}
-void loop()
-{
-  if (!LIS)
-  {
-    Serial.println("LIS3DHTR didn't connect.");
-    while (1)
-      ;
-    return;
-  }
-  //3 axis
-  Serial.print("x:"); Serial.print(LIS.getAccelerationX()); Serial.print("  ");
-  Serial.print("y:"); Serial.print(LIS.getAccelerationY()); Serial.print("  ");
-  Serial.print("z:"); Serial.println(LIS.getAccelerationZ());
-  //temperature
-  Serial.print("temp:");
-  Serial.println(LIS.getTemperature());
-  delay(500); 
+void setup() {
+Serial.begin(9600);
+Serial.println("Serial monitor configured to 9600");
+Serial.println("---------------------------------");
 }
