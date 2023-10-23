e1782c2d58002d0429255cb488d530e911fb3519
Darlene Purnomo <darlene.purnomo@education.nsw.gov.au>
2023-10-23 15:02:05 +1100

    added loop code

--- MoveWatch/dp.accelerometer.ino
+++ MoveWatch/dp.accelerometer.ino
@@ -1,3 +1,9 @@
+//include libraries
+#include "LIS3DHTR.h"
+#include <Wire.h>
+LIS3DHTR<TwoWire> LIS; //IIC
+#define WIRE Wire
+
 void setup() {
 Serial.begin(9600);
 Serial.println("Serial monitor configured to 9600");
@@ -5,5 +11,9 @@ Serial.println("---------------------------------");
 }
 
 void loop() {
-  Serial.print("hi");
+  if (acceleration > ) {
+// do stuff if the condition is true
+} else {
+// do stuff if the condition is false
+}
 }
