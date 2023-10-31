75b49ca92d26e202e3327bfdc309a4e3df4c87c8
Jade Huang <jade.huang2@education.nsw.gov.au>
2023-11-01 10:00:18 +1100

    even more notes for darlene

--- MoveWatch/dp.accelerometer.ino
+++ MoveWatch/dp.accelerometer.ino
@@ -1,7 +1,11 @@
-/* sorry  dardar we deleted your code because i got the accelerometer working
+/* 
+> sorry  dardar we deleted your code because i got the accelerometer working
 + we also got the stepcounter working
 
 notes for you:
+? can you do the health bar since i did the accelerometer
+- download step counter library: arduino_bma456.h, link found on its sensor catalogue, download from github
+
 - download visual studio code for easier navigation when integrating code (b jones recommendation)
 - create folder to make functions to include in mainline to integrate all our code
 - then call those functions in the main folder
