9d7f3fe92e9ab6756eacc349b3e0c794ebb0c304
Jade Huang <jade.huang2@education.nsw.gov.au>
2023-11-01 09:56:16 +1100

    added more notes for darlene

--- MoveWatch/dp.accelerometer.ino
+++ MoveWatch/dp.accelerometer.ino
@@ -1,4 +1,6 @@
 /* sorry  dardar we deleted your code because i got the accelerometer working
++ we also got the stepcounter working
+
 notes for you:
 - download visual studio code for easier navigation when integrating code (b jones recommendation)
 - create folder to make functions to include in mainline to integrate all our code
